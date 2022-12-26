328d7970ee792ead61d6ca9e8a8b5a0d3a16a491
myeongseok kwak <49023743+oh-bba-ya@users.noreply.github.com>
2022-12-26 20:16:29 +0900

    Add : FPSGameModeBase [replace BP_GameModeBase with FPSGameModeBase]
    기존에 사용하던 BP_GameModeBase를 삭제하고 FPSGameModeBase C++ 클래스와 BP_FPSGameModeBase 클래스를 생성한 뒤 대체했다.

---
+++ Source/First_Project_FPS/Private/FPSGameModeBase.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSGameModeBase.h"
+#include "Blueprint/UserWidget.h"
+#include "MainWidget.h"
+
+
+void AFPSGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (mainWidget != nullptr) {
+		// mainWidget 블루프린트 파일을 메모리에 로드한다.
+		mainUI = CreateWidget<UMainWidget>(GetWorld(), mainWidget);
+
+		// 위젯이 메모리에 로드되면 뷰 포트에 출력한다.
+		if (mainUI != nullptr) {
+			mainUI->AddToViewport();
+		}
+	}
+
+}
+
+void AFPSGameModeBase::PrintScore()
+{
+	if (mainUI != nullptr) {
+		// ScoreData 텍스트 블록에 현재 점수 값을 입력한다.
+		mainUI->scoreData->SetText(FText::AsNumber(currentScore));
+	}
+}
+
+// 현재 점수를 계산하는 함수
+void AFPSGameModeBase::AddScore(int32 point)
+{
+	// 매개변수 point를 통해 넘겨받은 점수를 현재 점수에 누적시킨다.
+	currentScore += point;
+
+	PrintScore();
+}
+
+
---
+++ Source/First_Project_FPS/Public/FPSGameModeBase.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRST_PROJECT_FPS_API AFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	void AddScore(int32 point);
+
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UMainWidget>mainWidget;
+
+protected:
+	virtual void BeginPlay() override;
+
+
+private:
+	// 현재 점수 저장용 변수
+	int32 currentScore = 0;
+
+	// 현재 뷰 포트에 로드된 위젯 저장용 변수
+	class UMainWidget* mainUI;
+
+	void PrintScore();
+
+
+};
